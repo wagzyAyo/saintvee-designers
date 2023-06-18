e7ee91e8fa4b4bc86b78c84171074afb615a9dfd
ayo <olamidsipupo1995@gmail.com>
2023-06-18 00:39:57 -0700

    payment routes

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./payment.css"
+import { contextProp } from "../context/context";
 
 
-const success = () => {
+const success = (props) => {
+    const { totalAmount } = useContext(contextProp);
+    const subTotal = totalAmount();
+
     return (
         <div className='payment-container'>
-            <h2>Transaction successful</h2>
+
+            <h2>Payment instructions</h2>
+            <p>Make a payment  of <span>{subTotal}</span> to the account details below  </p>
+            <div className='details'>
+                <p>Account Number:
+                    0039874808</p>
+
+                <p className='p2'>Account Name:
+                    Victor Chuka Ezekiel</p>
+
+            </div>
+
         </div>
     )
 }
